9bfa02d3eaa29d3fcaafd9cb762c3766ce286641
A-Isco <A-Isco>
2022-06-15 18:27:16 +0200

    ssss

--- django_final_project/settings.py
+++ django_final_project/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_final_project',
         'USER': 'root',
-        'PASSWORD': 'klorets12',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
